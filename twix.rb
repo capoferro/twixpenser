eeccb250e48baf33ca8c13fc35959f8c4a1e2449
josiah <josiah@josiah-VAIO-OF-DOOOM.(none)>
2009-04-04 14:16:10 -0700

    Initial commit

--- twix.rb
+++ twix.rb
@@ -3,66 +3,103 @@
 require 'rubygems'
 gem 'twitter4r'
 require 'twitter'
+require 'yaml'
 #work around for "undefined method parse"
 require 'time'
 
+##
+# Read in everything twixpenser will need to post to the google form.
+##
+twixpenser = YAML::load_file('twix.conf.yml')
 
 ##
-form_url = 'http://spreadsheets.google.com/formResponse?formkey=cHVoZkhUX2ZWUmVLUW83d011UzdfUnc6MA..'
-twitter_account = 'jkagbills'
-twitter_pass = 'aznapt'
+# Check if the logfile already exists. If not, create it.
 ##
-
 if File.exists?('logfile')
   logfile = File.new('logfile', 'r+')
   lines = logfile.readlines
-  last_date = Time.parse(lines[lines.length-1])
+
+  line_number = lines.length-1
+  looking_for_date = true
+  while looking_for_date and line_number > 0
+    begin
+      looking_for_date = false
+      puts last_date = Time.parse(lines[line_number])
+    rescue
+      line_number -= 1
+      looking_for_date = true
+    end
+  end
+
+  if looking_for_date
+    puts "ERROR: DATE NOT FOUND IN logfile"
+    last_date = Time.parse('1/1/2009')
+  end
+
 else
   logfile = File.new('logfile', 'w')
   last_date = Time.parse('1/1/2009')
 end
 
-client = Twitter::Client.new(:login => twitter_account, :password => twitter_pass)
+##
+# Retrieve tweets
+##
+client = Twitter::Client.new(:login => twixpenser[:account], 
+                             :password => twixpenser[:pass])
 received_messages = client.messages(:received)
-# d <twitter_account> <cost>!item!description
+
+##
+# Process tweets
+##
+processed_tweets_flag = false
+tweets_to_file = []
 received_messages.each { |tweet|
+
+  ##
+  # Check when the last twix.rb was successfully run.
+  ##
   if last_date >= tweet.created_at
-    puts 'No new tweets!'
+    puts 'No more new tweets!'
+    break
+  ##
+  # If it's a help request, send back help reply
+  ##
+  elsif tweet.to_s.chomp == 'help'
+    client.message(:post, "Format for expense: d #{twixpenser[:account]} <cost>!<item>!<description>", tweet.sender.name)
     break
   end
   
   begin
+    ##
+    # Split the tweet on !s.  No ! allowed in the description.
+    ##
     split_tweet = tweet.text.split('!')
     cost = split_tweet[0].chomp
     item = split_tweet[1].chomp
     desc = split_tweet[2].chomp
+    if split_tweet.length > 3
+      client.message(:post, "\"!\" are not allowed in the description section.  Some data may not have been sorted.")
+    end
     user = tweet.sender.name
+
+    curl = "curl -s -d 'entry.1.single=#{item}&entry.2.single=#{desc}&entry.3.single=#{cost}&entry.4.group=#{user}&entry.5.single=#{tweet.text}' #{twixpenser[:url]} > twixlogs/curl#{tweet.created_at.strftime("%Y-%m-%dT%H:%M:%S")}.log"
+
+    puts tweet
+    processed_tweets_flag = true
+    tweets_to_file << tweet
+    Kernel.system(curl)
   rescue
     client.message(:post, "Invalid: #{tweet}", tweet.sender.screen_name)
-    break
-  end
-  curl = "curl -ds 'entry.1.single=#{item}&entry.2.single=#{desc}&entry.3.single=#{cost}&entry.4.group=#{user}&entry.5.single=#{tweet.text}' #{form_url}" 
+    client.message(:post, "d #{twixpenser[:account]} <cost>!<item>!<description>", tweet.sender.screen_name)
+    tweets_to_file << 'ERROR: ' + tweet.to_s
+    processed_tweets_flag = true
+  end 
 
-  puts curl
-  Kernel.system(curl)
 }
 
-flag = false
-tweets_to_file = []
-received_messages.each{ |tweet|
-  if last_date >= tweet.created_at
-    break
-  end
-  flag = true
-  tweets_to_file << tweet
-}
-if flag
+if processed_tweets_flag
   logfile.puts 'vvvvvvvvvvvvvvvvv'
-  puts
-  puts
-  puts 'Processed:'
   tweets_to_file.each { |tweet|
-    puts "\t#{tweet}"
     logfile.puts tweet
   }
   logfile.puts '--retrieved at:--'
